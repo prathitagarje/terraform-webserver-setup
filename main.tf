3f5d96660753b1a6efc6736f36b9ff1388856c9d
Atul Kamble <atul_kamble@hotmail.com>
2026-02-11 09:03:46 +0530

    code

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.22.1"
+      version = "6.31.0"
     }
   }
 }
@@ -87,7 +87,7 @@ resource "aws_security_group" "http_server_sg" {
 }
 
 resource "aws_instance" "http_server" {
-  ami                         = "ami-0fa3fe0fa7920f68e"
+  ami                         = "ami-0c1fe732b5494dc14"
   instance_type               = "t3.medium"
   key_name                    = aws_key_pair.generated_key.key_name
   subnet_id                   = data.aws_subnets.default.ids[0]
